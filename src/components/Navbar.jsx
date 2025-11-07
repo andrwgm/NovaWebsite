2cd4598e57275b1cd9dfdee8df4eaf138152eace
andrwgm <93148812+andrwgm@users.noreply.github.com>
2025-11-07 11:38:34 +0100

    ➕ADD: Navbar made responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,25 @@ import { Menubar } from 'primereact/menubar';
 import { Image } from 'primereact/image';
 import { Button } from 'primereact/button';
 
-const items = [
-  { label: 'Families', url: '/hola'},
-  { label: 'Assesments'},
-  { label: 'About us'},
-  { label: 'Resources'},
-  { label: 'Nova careers'},
-  // { label: 'Nova careers', icon: 'pi pi-envelope' },
-];
-
 export default function Navbar({ activeIndex, onTabChange }) {
+  const handleContactClick = () => {
+    document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const items = [
+    { label: 'Families', url: '/hola'},
+    { label: 'Assesments'},
+    { label: 'About us'},
+    { label: 'Resources'},
+    { label: 'Nova careers'},
+    {
+      label: 'Get in contact',
+      icon: 'pi pi-send',
+      className: 'menu-contact-item',
+      command: handleContactClick,
+    }
+  ];
+
   return (
     <div className="topbar">
       <Menubar 
@@ -23,8 +32,8 @@ export default function Navbar({ activeIndex, onTabChange }) {
           <a href="/">
             <Image src="/images/topbar_logo.png" />
           </a>
-        } 
-        end={<Button label="Get in contact" icon="pi pi-send" iconPos="right" />} 
+        }
+        end={<Button label="Get in contact" icon="pi pi-send" iconPos="right" onClick={handleContactClick} />} 
       />
     </div>
   );
